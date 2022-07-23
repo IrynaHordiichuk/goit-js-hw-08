7cf6ac42ac2d273a53235ec09a5120a388ad3314
Iryna Hordiichuk <mason.iryna2@gmail.com>
2022-07-23 14:36:16 +0100

    adds gallery and Vimeo player
    task 1 and 2 of the homework 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryDivRef = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.map( item =>
+     `<li class="list__item"><a class="gallery__item" href="${item.original}">
+<img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a></li>`).join(' ');
+
+galleryDivRef.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+new SimpleLightbox('.gallery a', {captionsData: "alt", captionDelay: 250});
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
 
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const KEY = "videoplayer-current-time";
+
+function onPlay({seconds}) {
+
+localStorage.setItem(KEY, seconds);
+
+};
+const time = localStorage.getItem(KEY);
+console.log(time);
+
+if(time !== null){
+ player.setCurrentTime(time);
+}
